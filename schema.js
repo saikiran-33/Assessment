14417b9f5b35ff176ad2c1e54e5847629155adcd
BURRA SAI KIRAN <burrasaikiran174@gmail.com>
2025-01-29 21:29:55 +0530

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+});
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
